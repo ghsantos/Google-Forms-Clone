8d8a6babe30c1bb4e5974950af646d0b37743297
ghsantos <ghsantos933@gmail.com>
2023-06-28 19:05:18 -0300

    add forms UI

--- app/checkout/delivery.tsx
+++ app/checkout/delivery.tsx
@@ -1,22 +1,72 @@
-import React from 'react'
-import { Link, useRouter } from 'expo-router'
-import { Text, View } from 'react-native'
-import { Button } from 'react-native-paper'
+import React, { useState } from 'react'
+import { useRouter } from 'expo-router'
+import { ScrollView } from 'react-native'
+import {
+  Button,
+  Card,
+  RadioButton,
+  TextInput,
+  useTheme,
+} from 'react-native-paper'
 
 export default function DeliveryDetails() {
   const router = useRouter()
+  const theme = useTheme()
+
+  const [shipping, setShipping] = useState('free')
 
   const nextPage = () => {
     router.push('/checkout/payment')
   }
 
   return (
-    <View>
-      <Text>Delivery details</Text>
+    <ScrollView
+      contentContainerStyle={{
+        gap: 15,
+        width: '100%',
+        maxWidth: 550,
+        alignSelf: 'center',
+      }}
+      showsVerticalScrollIndicator={false}
+    >
+      <Card style={{ backgroundColor: theme.colors.background }}>
+        <Card.Title title="Deliver address" titleVariant="titleLarge" />
+        <Card.Content style={{ gap: 10 }}>
+          <TextInput
+            placeholder="City"
+            label="City"
+            style={{ backgroundColor: theme.colors.background }}
+          />
+          <TextInput
+            placeholder="Postal Code"
+            label="Postal Code"
+            style={{ backgroundColor: theme.colors.background }}
+          />
+          <TextInput
+            placeholder="Address"
+            label="Address"
+            style={{ backgroundColor: theme.colors.background }}
+          />
+        </Card.Content>
+      </Card>
+
+      <Card style={{ backgroundColor: theme.colors.background }}>
+        <Card.Title title="Shipping options" titleVariant="titleLarge" />
+        <Card.Content style={{ gap: 10 }}>
+          <RadioButton.Group
+            value={shipping}
+            onValueChange={value => setShipping(value)}
+          >
+            <RadioButton.Item label="Free" value="free" />
+            <RadioButton.Item label="Fast" value="fast" />
+            <RadioButton.Item label="Same day" value="same_day" />
+          </RadioButton.Group>
+        </Card.Content>
+      </Card>
 
       <Button onPress={nextPage} mode="contained">
         Next
       </Button>
-    </View>
+    </ScrollView>
   )
 }
--- app/checkout/payment.tsx
+++ app/checkout/payment.tsx
@@ -1,22 +1,60 @@
 import { useRouter } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
-import { Button } from 'react-native-paper'
+import { ScrollView, View } from 'react-native'
+import {
+  Button,
+  Card,
+  Checkbox,
+  Switch,
+  Text,
+  TextInput,
+  useTheme,
+} from 'react-native-paper'
 
 export default function PaymentDetails() {
   const router = useRouter()
+  const theme = useTheme()
 
   const nextPage = () => {
     router.push('/')
   }
 
   return (
-    <View>
-      <Text>Payment details</Text>
+    <ScrollView
+      contentContainerStyle={{
+        gap: 15,
+        width: '100%',
+        maxWidth: 550,
+        alignSelf: 'center',
+      }}
+      showsVerticalScrollIndicator={false}
+    >
+      <Card style={{ backgroundColor: theme.colors.background }}>
+        <Card.Title title="Payment details" titleVariant="titleLarge" />
+        <Card.Content style={{ gap: 10 }}>
+          <TextInput
+            placeholder="4242 4242 4242 4242"
+            label="Card number"
+            style={{ backgroundColor: theme.colors.background }}
+          />
+          <TextInput
+            placeholder="mm/yyyy"
+            label="Expiration date"
+            style={{ backgroundColor: theme.colors.background }}
+          />
+          <TextInput
+            placeholder="Security code"
+            label="Security code"
+            style={{ backgroundColor: theme.colors.background }}
+          />
+
+          <Checkbox.Item label="Save payment information" status="unchecked" />
+        </Card.Content>
+      </Card>
 
       <Button onPress={nextPage} mode="contained">
         Submit
       </Button>
-    </View>
+    </ScrollView>
   )
 }
